2e2255d63e67ac27a1f0b250d998519bbc5bfb74
Bruno Bieth <biethb@gmail.com>
2025-08-28 13:56:29 +0200

    add shell.nix & more nixos script compat

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # The Python environment with pyyaml included
+  pythonWithPackages = pkgs.python3.withPackages(ps: with ps; [
+    pyyaml
+  ]);
+
+  makeDist = pkgs.writeShellScriptBin "druid.makeDist" ''
+    ${pkgs.maven}/bin/mvn install -Dcheckstyle.skip=true -DskipTests -Pdist
+  '';
+in
+
+pkgs.mkShell {
+  # This makes the python interpreter with the packages available in your PATH
+  buildInputs = [
+    makeDist
+    pkgs.maven
+    pythonWithPackages
+  ];
+
+  shellHook = ''
+    >&2 echo "Welcome to the Druid shell, here are some useful commands:"
+    >&2 echo "* druid.makeDist - make a distribution"
+  '';
+}
